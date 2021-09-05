08bae7d64f80821367a1d13c740cc2d7c4fa3449
Luisa Quintian <luisaquintian@hotmail.com>
2021-09-05 20:55:06 -0300

    Added option to check tasks as completed

--- Project-toDo.app/script.js
+++ Project-toDo.app/script.js
@@ -12,34 +12,27 @@ function addNewItem() {
     //1 - Get data from the input
     const newItem = document.getElementById('new-task-input').value
 
-    //2 - verificar se foi preenchido
+    //2 - Check if the input was filled
     if (newItem === "") {return}
 
     //3 - Put the data into the list
-        //3.1 - create a <li> element
-    const li = document.createElement('li')
-        //3.2 - create a text node
-    const node = document.createTextNode(`${newItem}`)
-        //3.3 - append the thext node to the <li> element'
-    li.appendChild(node)
-        //3.4 - append the new element to an existing element inthe html
+    const taskEl = document.createElement("li")
+    taskEl.innerHTML = newItem
     const element = document.getElementById('listUl')
-    element.appendChild(li)
+    element.appendChild(taskEl)
+    taskEl.classList.add('listItem')
+
+    //4 - Add event listener 
+    taskEl.addEventListener('click', () => {
+        taskEl.classList.toggle('checked')
+    })
 
     //4 - Clean the input
     document.getElementById('new-task-input').value = ''
 
     //5 - Remove the first item
-    document.getElementById('firstItem').remove() 
+    document.querySelector('.firstItem').remove() 
 }
-
-function checked() {
-    document.getElementById('firstItem').classList.toggle('checked')
-}
-
-
-
-
 
 function toggle() {
     document.getElementById('modal').classList.toggle('hidden')
@@ -77,3 +70,14 @@ function colorSwitch(color) {
     
     document.getElementById('modal').classList.add('hidden')
 }
+
+
+    //     //3.1 - create a <li> element
+    //     const li = document.createElement('li')
+    //     //3.2 - create a text node
+    // const node = document.createTextNode(`${newItem}`)
+    //     //3.3 - append the thext node to the <li> element'
+    // li.appendChild(node)
+    //     //3.4 - append the new element to an existing element inthe html
+    // const element = document.getElementById('listUl')
+    // element.appendChild(li)
