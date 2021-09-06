7ee9f96c5c57d03fe89329fc81fc052360f4a099
Luisa Quintian <luisaquintian@hotmail.com>
2021-09-06 19:33:54 -0300

    Added code to save data in the local storage

--- Project-toDo.app/script.js
+++ Project-toDo.app/script.js
@@ -5,37 +5,54 @@ window.onload = function() {
     } else {
         colorSwitch(getColor)
     }
-  };
 
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
+    if(tasks) {
+        tasks.forEach(task => {
+            addNewItem(task)
+        })
+    } 
+  }
 
-function addNewItem() {
-    //1 - Get data from the input
-    const newItem = document.getElementById('new-task-input').value
+function addNewItem(task) {
+    //1 - Get data from the input and from the local storage
+    let newItem = document.getElementById('new-task-input').value
+
+    if (task) {newItem = task.text}
+    if (task && task.checked) {
+        taskEl.classList.add('checked')
+    }
 
     //2 - Check if the input was filled
     if (newItem === "") {return}
 
     //3 - Put the data into the list
     const taskEl = document.createElement("li")
-    taskEl.innerHTML = `${newItem} <a onclick='removeEl(this)' href="#">X</a>`
+    taskEl.innerHTML = `${newItem} <a onclick='removeEl(this)' href="#">X</a>` //Remove
     const element = document.getElementById('listUl')
     element.appendChild(taskEl)
     taskEl.classList.add('listItem')
 
-    //4 - Add event listener 
+    //Check
     taskEl.addEventListener('click', () => {
         taskEl.classList.toggle('checked')
+        updateLS()
     })
 
     //4 - Clean the input
     document.getElementById('new-task-input').value = ''
 
     //5 - Remove the first item
-    document.querySelector('.firstItem').remove() 
+    if (document.querySelector('.firstItem')) {
+    document.querySelector('.firstItem').remove()}
+
+    //6 - Update local storage
+    updateLS()
 }
 
 function removeEl(e) {
     e.parentElement.remove()
+    updateLS()
 }
 
 function toggle() {
@@ -70,18 +87,18 @@ function colorSwitch(color) {
             bodyColor.classList.add('pink');
             localStorage.setItem("color", "pink")
     }
-    // toggle()
-    
     document.getElementById('modal').classList.add('hidden')
 }
 
+function updateLS() {
+    const tasksEl = document.querySelectorAll('li')
+    const tasks = []
 
-    //     //3.1 - create a <li> element
-    //     const li = document.createElement('li')
-    //     //3.2 - create a text node
-    // const node = document.createTextNode(`${newItem}`)
-    //     //3.3 - append the thext node to the <li> element'
-    // li.appendChild(node)
-    //     //3.4 - append the new element to an existing element inthe html
-    // const element = document.getElementById('listUl')
-    // element.appendChild(li)
+    tasksEl.forEach(taskEl => {
+        tasks.push({
+            text: taskEl.innerText,
+            completed: taskEl.classList.contains('checked')
+        })
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
